3009adb7593dd0c6635bc2260cf8b327db702ac7
Parham2002 <parhamm2002@gmail.com>
2022-08-21 16:14:30 +0100

    Added some info on readme, and comments on .js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # first-calculator
+
+This is my first visual calculator project. It works by taking inputs from the buttons on the website, and taking them through few Javascript functions to get the desired output on the page. 
+
+I have used array iterators, function constructors, switch cases, along with HTML, CSS, and SCSS to do this project.
--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,14 +1,20 @@
 "use strict";
 
+// Calculator project
+// Taking inputs from the HTML code
 var inputArea = document.querySelector(".input-area__box");
-var buttons = Array.from(document.querySelectorAll(".buttons__item"));
+var buttons = Array.from(document.querySelectorAll(".buttons__item")); // Using a function constructor since usage of eval()
+// is not recommended for calculations
 
 var calculator = function calculator(values) {
   return new Function("return " + values)();
-};
+}; // Using .map iterator to make a new array out of buttons array
+
 
 buttons.map(function (button) {
+  // Using .addEventListener to respond to events occuring on the page
   button.addEventListener("click", function (ans) {
+    // Using a switch case to filter out some button inputs
     switch (ans.target.innerText) {
       case "AC":
         inputArea.value = "";
@@ -20,6 +26,7 @@ buttons.map(function (button) {
 
       default:
         inputArea.value += ans.target.innerText;
+        break;
     }
   });
 });
