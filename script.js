07658f7c13451582e25e506091ae1746a8427aaf
Parham2002 <parhamm2002@gmail.com>
2022-08-21 16:12:47 +0100

    added info on readme, added comments for .js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+// Calculator project
+
+// Taking inputs from the HTML code
 let inputArea = document.querySelector(".input-area__box")
 let buttons = Array.from(document.querySelectorAll(".buttons__item"))
 
+// Using a function constructor since usage of eval()
+// is not recommended for calculations
 const calculator = (values) => {
     return new Function("return " + values)();
 }
 
-
+// Using .map iterator to make a new array out of buttons array
 buttons.map( button => {
+    // Using .addEventListener to respond to events occuring on the page
     button.addEventListener("click", (ans) => {
+        // Using a switch case to filter out some button inputs
         switch (ans.target.innerText){
             case "AC":
                inputArea.value= "";
@@ -17,6 +24,7 @@ buttons.map( button => {
                 break;
             default:
                 inputArea.value += ans.target.innerText;
+                break;
         }
     })
 })
